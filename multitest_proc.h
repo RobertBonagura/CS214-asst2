dd11435acfce99beafcfc2a333493cb65929b9ff
Peter E. Tilton <pet27@python.cs.rutgers.edu>
2019-11-22 02:22:12 -0500

    name changes and serious rewrites to match deliverable specs

---
+++ multitest_proc.h
@@ -0,0 +1,23 @@
+#ifndef _PROC_H
+#define _PROC_H
+#include <unistd.h>
+#include "multitest.h"
+
+#define _search(list, size, parallels, target) search((list), (size), (parallels), (target))
+#define SEARCH_MODE 1
+
+// Structs
+typedef struct _Sublist {
+	int* list;
+	int size;
+} Sublist;
+
+// Functin Prototypes
+
+int search(int* list, int size, int parallels, int target);
+int* generate_sublist_indices(int list_size, int n_procs);
+void show_indices(int* sublist_indices, int n_procs);
+void create_procs(int n_procs, int* list, int* sublist_indices, int list_size, int target, pid_t* children);
+Sublist gen_sublist(int* list, int list_size, int* sublist_indices, int n_procs, int proc_id,  int offset);
+
+#endif
---
+++ multitest_thread.h
@@ -0,0 +1,23 @@
+#ifndef _THREAD_H
+#define _THREAD_H
+#include "multitest.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define _search(list, size, parallels, target) search((list), (size), (parallels), (target))
+#define SEARCH_MODE 2
+#define THREAD_DEBUG 0
+
+typedef struct _thread_atts {
+	int* list;
+	int list_size;
+	int target;
+	int result;
+} thread_atts;
+
+int search(int* list, int list_size, int n_threads, int target);
+void* my_thread(void* arg);
+int linear_search(int* list, int list_size, int target);
+
+#endif
